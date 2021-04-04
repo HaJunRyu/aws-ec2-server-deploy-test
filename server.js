686bb7cf11a23d59eed82b7276101d0a2a38cc05
HaJunRyu <hjr4557@naver.com>
2021-04-04 15:52:08 +0900

    Feat: image_upload test with aws-server modified:   package-lock.json modified:   package.json modified:   server.js

--- server.js
+++ server.js
@@ -14,14 +14,42 @@ AWS.config.region = 'ap-northeast-2';
 const s3 = new AWS.S3();
 
 app.all('/', (req, res) => {
-  res.send('connected aws ec2 server');
+  res.send(`
+  <html>
+      <body>
+        <form enctype="multipart/form-data" method="post" action="/signup">
+          <input type="file" name="image"/><button type="submit">전송</button>
+        </form>
+      </body>
+    </html>`);
 });
 
-app.get('/login', (req, res) => {
-  res.json({
-    email: 'fbgkwns@gmail.com',
-    nickname: '하준',
-    profilePhoto: 'https://avatars.githubusercontent.com/u/71176945?v=4'
+// app.get('/login', (req, res) => {
+//   connection.query('SELECT * from users', (error, rows) => {
+//     if (error) throw error;
+//     console.log('User info is: ', rows);
+//     res.json(rows);
+//   });
+// });
+
+app.post('/signup', (req, res) => {});
+
+app.post('/image_upload', (req, res) => {
+  const form = new formidable.IncomingForm();
+  form.parse(req, (err, fields, files) => {
+    console.log(files.image);
+    const params = {
+      Bucket: 'aws-devfolio',
+      Key: files.image.name,
+      ACL: 'public-read',
+      Body: require('fs').createReadStream(files.image.path)
+    };
+    s3.upload(params, (err, data) => {
+      let result = '';
+      if (err) result = 'Fail';
+      else result = data.location;
+      res.send(result);
+    });
   });
 });
 
